a8e4d3f082aafca0e4a30ac57f153dcb594fbc4a
Sylvi Mosier <omosier2@huskers.unl.edu>
2025-08-28 12:58:47 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Sylvi Mosier
+ * Date: 2025/08/28
+ *
+ * Prints out the author's name
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("Sylvi Mosier\n");
+
+    return 0;
+}
